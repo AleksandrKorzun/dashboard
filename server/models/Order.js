6a85bf5f0ffd5a1afebd5150d8926909e73b1255
unknown <oleksandr.korzun@holywater.tech>
2023-02-06 01:51:18 +0200

    add files

---
+++ server/models/Order.js
@@ -0,0 +1,22 @@
+import { model, Schema } from "mongoose";
+
+const OrderSchema = new Schema({
+  nameClient: String,
+  phone: Number,
+  city: String,
+  region: String,
+  nameProduct: String,
+  price: Number,
+  quantity: Number,
+  status: {
+    type: String,
+    enum: ["sent", "success", "cancel", "change", "return"],
+    default: "sent",
+  },
+  
+},
+{ timestamps: true }
+);
+
+const Order = model("Order", OrderSchema);
+export default Order;
